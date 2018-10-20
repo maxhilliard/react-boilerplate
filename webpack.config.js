27eda2df5a2e22db7c42c91d9f80ce302be56df6
maxhilliard <mx.hilliard@gmail.com>
2018-10-20 23:44:22 +0100

    UNTESTED - webpack config for image import

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,15 @@ module.exports = {
                         loader: 'postcss-loader',
                     },
                 ],
+            }, {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000,
+                        name: 'images/[hash]-[name].[ext]',
+                    },
+                }],
             },
         ],
     },
